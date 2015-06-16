ae18882403fe16566d5b086084384b47fe40297c
Michele Cantelli <emmekappa@gmail.com>
2015-06-16 14:15:23 +0000

    Spark master environment

--- jobserver_start.sh
+++ jobserver_start.sh
@@ -5,7 +5,10 @@ echo 127.0.0.1 $HOSTNAME >> /etc/hosts
 
 #source $SPARK_HOME/conf/spark-env.sh
 echo "" >> $JOBSERVER_CONF
-export MASTER="spark://${SPARK_MASTER_PORT_7077_TCP_ADDR}:${SPARK_MASTER_PORT_7077_TCP_PORT}"
+
+[ ! "$SPARK_MASTER" ] && SPARK_MASTER="spark://${SPARK_MASTER_PORT_7077_TCP_ADDR}:${SPARK_MASTER_PORT_7077_TCP_PORT}"
+
+#export MASTER="spark://${SPARK_MASTER_PORT_7077_TCP_ADDR}:${SPARK_MASTER_PORT_7077_TCP_PORT}"
 echo "spark { master = \"${MASTER}\" }" >> $JOBSERVER_CONF 
 $JOBSERVER_HOME/_server_start.sh
 tail -f /dev/null
