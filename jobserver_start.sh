26142dcd4017e9f5257b6cecc6b77fddefbe8ff5
Michele Cantelli <michele.cantelli@yoox.com>
2015-06-17 01:41:49 +0200

    Adding mesos

--- jobserver_start.sh
+++ jobserver_start.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-export SPARK_LOCAL_IP=`awk 'NR==1 {print $1}' /etc/hosts`
-/remove_alias.sh # problems with hostname alias, see https://issues.apache.org/jira/browse/SPARK-6680
-echo 127.0.0.1 $HOSTNAME >> /etc/hosts 
+#export SPARK_LOCAL_IP=`awk 'NR==1 {print $1}' /etc/hosts`
+#/remove_alias.sh # problems with hostname alias, see https://issues.apache.org/jira/browse/SPARK-6680
+#echo 127.0.0.1 $HOSTNAME >> /etc/hosts 
 
 #source $SPARK_HOME/conf/spark-env.sh
 echo "" >> $JOBSERVER_CONF
 
-[ ! "$SPARK_MASTER" ] && SPARK_MASTER="spark://${SPARK_MASTER_PORT_7077_TCP_ADDR}:${SPARK_MASTER_PORT_7077_TCP_PORT}"
-echo "spark { master = \"${SPARK_MASTER}\" }" >> $JOBSERVER_CONF 
+[ ! "$MESOS_MASTER" ] && MESOS_MASTER="mesos://${MESOS_MASTER_PORT_5050_TCP_ADDR}:${MESOS_MASTER_PORT_5050_TCP_PORT}"
+echo "spark { master = \"${MESOS_MASTER}\" }" >> $JOBSERVER_CONF 
 $JOBSERVER_HOME/_server_start.sh
 tail -f /dev/null
